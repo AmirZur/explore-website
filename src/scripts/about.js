adee3df775a7ecab9f7d593f95a0fd47917e1f49
luvannaaa <91120391+luvannaaa@users.noreply.github.com>
2022-03-18 12:04:38 -1000

    add general venn diagram

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,3 +1,7 @@
+$('#first-title').circleType({radius: 300});
+
+$('#second-title').circleType({radius: 300});
+
 $('#cyber-title').circleType({radius: 300});
 
 $('#business-title').circleType({radius: 300});
